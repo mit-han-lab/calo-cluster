5e2111c56140923d85b1e0801ca32b3b4eaf775f
Alex Schuy <alexjschuy@gmail.com>
2020-10-07 15:25:16 +0000

    Add efficiency and purity.

--- hgcal_dev/clustering/meanshift.py
+++ hgcal_dev/clustering/meanshift.py
@@ -1,11 +1,10 @@
 from sklearn import cluster
-from torch import nn
 
 
-class MeanShift(nn.Module):
+class MeanShift():
     def __init__(self, bandwidth: float = None):
         self.clusterer = cluster.MeanShift(bandwidth=bandwidth)
 
-    def forward(self, x):
+    def cluster(self, x):
         labels = self.clusterer.fit_predict(x)
         return labels
