8ea7cea56d1de7dd45f15b9a2e31c2f46e9952b0
Alex Schuy <alexjschuy@gmail.com>
2021-04-07 19:21:42 +0000

    Fix intersections in weighted matching.

--- hgcal_dev/evaluation/studies/clustering_study.py
+++ hgcal_dev/evaluation/studies/clustering_study.py
@@ -1,6 +1,52 @@
 from hgcal_dev.evaluation.studies.base_study import BaseStudy
 from hgcal_dev.clustering.meanshift import MeanShift
 from tqdm import tqdm
+import numpy as np
+
+class IoUMatcher():
+    '''Match true and predicted clusters using IoU. Can optionally include weighting and use of semantic labels.'''
+    def __init__(self) -> None:
+        pass
+
+    def match(self, xi, yi, weights=None):
+        ''' xi: predicted cluster labels
+            yi: true cluster labels
+            weights: weight for each sample'''
+        if weights is None:
+            weights = np.ones_like(xi, dtype=np.float64)
+
+        xinstances = np.unique(xi)
+        xmapping = {k: idx for idx, k in enumerate(xinstances)}
+        xmatched = np.array([False] * xinstances.shape[0])
+        xareas = (np.broadcast_to(weights, (len(xinstances), len(weights)))
+                    * xi == xinstances[..., None]).sum(axis=1)
+
+        yinstances = np.unique(yi)
+        ymapping = {k: idx for idx, k in enumerate(yinstances)}
+        ymatched = np.array([False] * yinstances.shape[0])
+        yareas = (np.broadcast_to(weights, (len(yinstances), len(weights)))
+                    * yi == yinstances[..., None]).sum(axis=1)
+
+        xylabels = xi + (2 ** 32) * yi
+        xyinstances = np.unique(xylabels)
+        intersections = (np.broadcast_to(
+            weights, (len(xyinstances), len(weights))) * (xylabels == xyinstances[..., None]).astype(int)).sum(axis=1)
+        xyxinstances, xyyinstances = xyinstances % (
+            2 ** 32), xyinstances // (2 ** 32)
+
+        xyxmask = xi == xyxinstances[..., None]
+        xyxareas = (np.broadcast_to(
+            weights, (len(xyxinstances), len(weights))) * xyxmask).sum(axis=1)
+
+        xyymask = yi == xyyinstances[..., None]
+        xyyareas = (np.broadcast_to(
+            weights, (len(xyyinstances), len(weights))) * xyymask).sum(axis=1)
+
+        unions = xyxareas + xyyareas - intersections
+        ious = intersections / unions
+        indices = ious > 0.5
+        return xyxinstances[indices], xyyinstances[indices], ious[indices]
+
 
 class ClusteringStudy(BaseStudy):
 
@@ -23,3 +69,16 @@ class ClusteringStudy(BaseStudy):
                 results[k] += pq[k] / nevents
         
         return results
+
+def main():
+    xi = np.array([0, 0, 1, 1, 1])
+    yi = np.array([2, 2, 2, 1, 1])
+    weights = np.array([10, 1, 1, 1, 1])
+    matcher = IoUMatcher()
+    xclusters, yclusters, ious = matcher.match(xi, yi, weights)
+    print(f'xclusters={xclusters}')
+    print(f'yclusters={yclusters}')
+    print(f'ious={ious}')
+
+if __name__ == '__main__':
+    main()
--- hgcal_dev/metrics/instance.py
+++ hgcal_dev/metrics/instance.py
@@ -1,34 +1,6 @@
 import numpy as np
 from sklearn.metrics import jaccard_score
 
-
-def match_instances(labels, pred_labels, return_maps=True):
-    unique_labels = np.unique(labels)
-    unique_pred_labels = np.unique(pred_labels)
-    ious = np.zeros((unique_labels.shape[0], unique_pred_labels.shape[0]))
-    for i, truth_label in enumerate(unique_labels):
-        for j, pred_label in enumerate(unique_pred_labels):
-            _labels = labels.copy()
-            _labels[labels == truth_label] = 1
-            _labels[labels != truth_label] = 0
-            _pred_labels = labels.copy()
-            _pred_labels[pred_labels == pred_label] = 1
-            _pred_labels[pred_labels != pred_label] = 0
-            ious[i, j] = jaccard_score(_labels, _pred_labels, average='binary')
-    if return_maps:
-        truth_to_pred = {k: v for k, v in zip(
-            unique_labels, ious.argmax(axis=1))}
-        pred_to_truth = {v: k for k, v in truth_to_pred.items()}
-        return truth_to_pred, pred_to_truth, ious.max(axis=1)
-    else:
-        return ious.max(axis=1)
-
-
-def mIoU(labels, pred_labels):
-    ious = match_instances(labels, pred_labels, return_maps=False)
-    return ious.mean()
-
-
 class PanopticQuality:
 
     def __init__(self, *, num_classes=1, ignore_index=None, semantic=True):
@@ -99,7 +71,7 @@ class PanopticQuality:
             xylabels = xik[xymask] + (2 ** 32) * yik[xymask]
             xyinstances = np.unique(xylabels)
             intersections = (np.broadcast_to(
-                xyweights, (len(xyinstances), len(xyweights))) * xylabels == xyinstances[..., None]).sum(axis=1)
+                xyweights, (len(xyinstances), len(xyweights))) * (xylabels == xyinstances[..., None]).astype(int)).sum(axis=1)
             xyxinstances, xyyinstances = xyinstances % (
                 2 ** 32), xyinstances // (2 ** 32)
 
