fix spvcnn num_training_steps calculation

hgcal_dev/evaluation/metrics/instance.py
@@ -24,7 +24,7 @@ class PanopticQuality:
 
     def add(self, outputs, targets, weights=None):
         _xyxinstances, _xyyinstances, _ious, _xmatched, _ymatched, _xareas, _yareas, _xmapping, _ymapping, _intersections = iou_match(
-            outputs, targets, weights=weights, threshold=0.5, semantic=self.semantic, ignore_index=self.ignore_index, ignore_class_labels=self.ignore_class_labels)
+            outputs, targets, weights=weights, threshold=0.5, semantic=self.semantic, ignore_index=self.ignore_index, ignore_class_labels=self.ignore_class_labels, num_classes=self.num_classes)
         for k in range(self.num_classes):
             if k not in _xyxinstances:
                 self.tps[k], self.wtps[k], self.ious[k], self.fps[k], self.fns[k], self.wfps[k], self.wfns[k] = - \
@@ -60,7 +60,7 @@ class PanopticQuality:
         return {'sq': sq, 'rq': rq, 'pq': pq, 'tq': tq, 'wrq': wrq, 'wpq': wpq, 'wtq': wtq}
 
 
-def iou_match(outputs, targets, weights=None, threshold=0.5, semantic=False, ignore_index=None, ignore_class_labels=None, match_highest=False):
+def iou_match(outputs, targets, num_classes, weights=None, threshold=0.5, semantic=False, ignore_index=None, ignore_class_labels=None, match_highest=False):
     ''' xi: predicted cluster labels
         yi: true cluster labels
         weights: weight for each sample
@@ -95,7 +95,7 @@ def iou_match(outputs, targets, weights=None, threshold=0.5, semantic=False, ign
     _xmapping = {}
     _ymapping = {}
     _intersections = {}
-    for k in range(len(np.unique(np.concatenate([xs, ys])))):
+    for k in range(num_classes):
 
         if ignore_class_labels is not None and k in ignore_class_labels:
             continue

hgcal_dev/evaluation/studies/clustering_study.py
@@ -9,13 +9,81 @@ from hgcal_dev.evaluation.utils import get_palette
 from scipy.optimize import basinhopping
 from tqdm import tqdm
 
+from hgcal_dev.evaluation.experiments.base_experiment import BaseExperiment
 
 class ClusteringStudy(BaseStudy):
 
-    def __init__(self, experiment, energy_name='energy', clusterer=None) -> None:
+    def __init__(self, experiment: BaseExperiment, energy_name: str = 'energy', clusterer=None) -> None:
         assert experiment.task == 'panoptic' or experiment.task == 'instance'
         self.energy_name = energy_name
         super().__init__(experiment, clusterer)
+
+    def get_clusters(self, nevents, split, match_highest=False):
+        events = self.experiment.get_events(split=split, n=nevents)
+        truth_clusters = []
+        pred_clusters = []
+        truth_hits = []
+        pred_hits = []
+        for event in tqdm(events):
+            xi = self.clusterer.cluster(event)
+            yi = event.input_event[event.instance_label].values
+            df = event.input_event
+            df['weta'] = df['eta'] * df['energy']
+            df['wphi'] = df['phi'] * df['energy']
+            result = df.groupby([event.instance_label])[
+                ['energy', 'weta', 'wphi']].agg(['mean', 'count'])
+            energy = result[('energy', 'mean')]
+            energy.name = 'energy'
+            nconstituents = result[('energy', 'count')]
+            nconstituents.name = 'nconstituents'
+            eta = result[('weta', 'mean')] / energy
+            eta.name = 'eta'
+            phi = result[('wphi', 'mean')] / energy
+            phi.name = 'phi'
+            if event.weight_name:
+                weights = event.input_event[event.weight_name].values
+            else:
+                weights = None
+            if self.clusterer.use_semantic:
+                xs = event.pred_class_labels
+                ys = event.input_event[event.class_label].values
+                outputs = (xs, xi)
+                targets = (ys, yi)
+            else:
+                outputs = xi
+                targets = yi
+            matched_pred, matched_truth, *_ = iou_match(
+                outputs, targets, weights=weights, ignore_class_labels=(self.clusterer.ignore_class_label,), semantic=self.clusterer.use_semantic, match_highest=match_highest, num_classes=self.experiment.num_classes)
+            if 1 in matched_pred:
+                matched_pred = matched_pred[1]
+                matched_truth = matched_truth[1]
+            else:
+                matched_pred = matched_pred[0]
+                matched_truth = matched_truth[0]
+            for pred_label, truth_label in zip(matched_pred, matched_truth):
+                _truth_hits = event.input_event[yi == truth_label].copy()
+                _pred_hits = event.input_event[xi == pred_label].copy()
+                c_energy = _truth_hits['energy'].sum()
+                _truth_hits['weta'] = _truth_hits['eta'] * _truth_hits['energy']
+                _truth_hits['wphi'] = _truth_hits['phi'] * _truth_hits['energy']
+                c_eta = _truth_hits['weta'].sum() / c_energy
+                c_phi = _truth_hits['wphi'].sum() / c_energy
+                c_n_constituents = len(_truth_hits)
+                c_pred_energy = _pred_hits['energy'].sum()
+                c_response =  c_pred_energy / c_energy
+                truth_clusters.append([c_energy, c_eta, c_phi, c_n_constituents, c_pred_energy, c_response])
+                _pred_hits['weta'] = _pred_hits['eta'] * _pred_hits['energy']
+                _pred_hits['wphi'] = _pred_hits['phi'] * _pred_hits['energy']
+                c_pred_eta = _pred_hits['weta'].sum() / c_pred_energy
+                c_pred_phi = _pred_hits['wphi'].sum() / c_pred_energy
+                c_pred_n_constituents = len(_pred_hits)
+                pred_clusters.append([c_pred_energy, c_pred_eta, c_pred_phi, c_pred_n_constituents])
+                truth_hits.append(_truth_hits)
+                pred_hits.append(_pred_hits)
+
+        truth_clusters = pd.DataFrame(truth_clusters, columns=['energy', 'eta', 'phi', 'n_constituents', 'pred_energy', 'response'])
+        pred_clusters = pd.DataFrame(pred_clusters, columns=['energy', 'eta', 'phi', 'n_constituents'])
+        return truth_clusters, truth_hits, pred_clusters, pred_hits
 
     def response(self, nevents=100, nbins=21, lo=0, hi=20, splits=('val',), out_dir='.', match_highest=False):
         out_dir = self.out_dir / out_dir
@@ -29,7 +97,7 @@ class ClusteringStudy(BaseStudy):
             data_dict[split] = {}
             events = self.experiment.get_events(split=split, n=nevents)
             cluster_dfs, event_dfs = F.response(
-                events, self.clusterer, match_highest)
+                events, self.clusterer, match_highest, num_classes=self.experiment.num_classes)
             data_dict[split]['cluster_dfs'] = cluster_dfs
             data_dict[split]['event_dfs'] = event_dfs
             for k in cluster_dfs:
@@ -109,7 +177,7 @@ class ClusteringStudy(BaseStudy):
             size = None
             weights = None
         _pred_clusters, _truth_clusters, *_ = iou_match(
-            xi, yi, weights=weights)
+            xi, yi, weights=weights, num_classes=self.experiment.num_classes)
         for k in _pred_clusters:
             pred_clusters = _pred_clusters[k].astype(str)
             truth_clusters = _truth_clusters[k].astype(str)

hgcal_dev/evaluation/studies/functional.py
@@ -6,7 +6,7 @@ from hgcal_dev.evaluation.metrics.instance import PanopticQuality, iou_match
 from tqdm import tqdm
 
 
-def cluster_width(events, clusterer, match_highest):
+def cluster_width(events, clusterer, match_highest, num_classes):
     for i, event in tqdm(enumerate(events)):
         xi = clusterer.cluster(event)
         yi = event.input_event[event.instance_label].values
@@ -26,7 +26,7 @@ def cluster_width(events, clusterer, match_highest):
             targets = yi
         
         matched_pred, matched_truth, *_ = iou_match(
-            outputs, targets, weights=weights, ignore_class_labels=(clusterer.ignore_class_label,), semantic=clusterer.use_semantic, match_highest=match_highest)
+            outputs, targets, weights=weights, ignore_class_labels=(clusterer.ignore_class_label,), semantic=clusterer.use_semantic, match_highest=match_highest, num_classes=num_classes)
         for k in matched_pred:
             if clusterer.use_semantic:
                 yik = yi[ys == k]
@@ -41,7 +41,7 @@ def cluster_width(events, clusterer, match_highest):
             yim_mask = yik == matched_truth[k][..., None]
             xim_mask = xik == matched_pred[k][..., None]
 
-def response(events, clusterer, match_highest):
+def response(events, clusterer, match_highest, num_classes):
     responses = defaultdict(list)
     energies = defaultdict(list)
     unmatched_true = defaultdict(lambda: np.zeros(len(events)))
@@ -62,7 +62,7 @@ def response(events, clusterer, match_highest):
             outputs = xi
             targets = yi
         matched_pred, matched_truth, *_ = iou_match(
-            outputs, targets, weights=weights, ignore_class_labels=(clusterer.ignore_class_label,), semantic=clusterer.use_semantic, match_highest=match_highest)
+            outputs, targets, weights=weights, ignore_class_labels=(clusterer.ignore_class_label,), semantic=clusterer.use_semantic, match_highest=match_highest, num_classes=num_classes)
         for k in matched_pred:
             if clusterer.use_semantic:
                 yik = yi[ys == k]
@@ -107,7 +107,7 @@ def response(events, clusterer, match_highest):
     return cluster_dfs, event_dfs
 
 
-def make_bins(x, y, bin_edges, use_standard_error: bool = True):
+def make_bins(x, y, bin_edges, use_standard_error: bool = True, use_median: bool = False):
     "find mean and std error of y within bins of x determined by bin_edges"
     nbins = len(bin_edges)
     means = np.zeros(nbins)
@@ -116,11 +116,15 @@ def make_bins(x, y, bin_edges, use_standard_error: bool = True):
     for i in range(nbins):
         mask = (bin_indices == i)
         y_bin = y[mask]
-        means[i] = np.mean(y_bin)
-        if use_standard_error:
-            errors[i] = np.std(y_bin) / (np.sum(mask))**0.5  # standard error
+        if use_median:
+            means[i] = np.median(y_bin)
+            errors[i] = np.abs(np.percentile(y_bin, 75) - np.percentile(y_bin, 25))
         else:
-            errors[i] = np.std(y_bin)
+            means[i] = np.mean(y_bin)
+            if use_standard_error:
+                errors[i] = np.std(y_bin) / (np.sum(mask))**0.5  # standard error
+            else:
+                errors[i] = np.std(y_bin)
     return means, errors
 
 def pq(event, use_weights, clusterer, task, ignore_class_labels):

hgcal_dev/training/criterion.py
@@ -116,17 +116,18 @@ def centroid_instance_loss(outputs, labels, subbatch_indices, weights, use_weigh
             M = unique_labels.shape[0]
 
             # Find mean of each instance and calculate L_pull.
-            L_pull = 0.0
-            for m, label in enumerate(unique_labels):
-                mask = subbatch_labels == label
-                Nm = mask.sum()
-                mu = subbatch_outputs[mask].mean(axis=0)
-                W = subbatch_weights[mask].mean(axis=0)
-                Ws[m] = W
-                mus[m] = mu
-                L_pull += (subbatch_weights[mask] * F.relu(torch.norm(mu - subbatch_outputs[mask], p=1, dim=1) - delta_v)**2).sum() / (M * Nm)
-            L_push = (F.relu(2 * delta_d - torch.norm(mus.unsqueeze(1) - mus, p=1, dim=2)).fill_diagonal_(0)**2).sum() / (M * (M - 1))
-            loss += (L_pull + L_push) / B
+            if M > 1:
+                L_pull = 0.0
+                for m, label in enumerate(unique_labels):
+                    mask = subbatch_labels == label
+                    Nm = mask.sum()
+                    mu = subbatch_outputs[mask].mean(axis=0)
+                    W = subbatch_weights[mask].mean(axis=0)
+                    Ws[m] = W
+                    mus[m] = mu
+                    L_pull += (subbatch_weights[mask] * F.relu(torch.norm(mu - subbatch_outputs[mask], p=1, dim=1) - delta_v)**2).sum() / (M * Nm)
+                L_push = (F.relu(2 * delta_d - torch.norm(mus.unsqueeze(1) - mus, p=1, dim=2)).fill_diagonal_(0)**2).sum() / (M * (M - 1))
+                loss += (L_pull + L_push) / B
         return loss
 
 class CentroidInstanceLoss(nn.Module):
@@ -145,7 +146,7 @@ class CentroidInstanceLoss(nn.Module):
             loss = 0.0
             unique_semantic_labels = torch.unique(semantic_labels)
             for semantic_label in unique_semantic_labels:
-                if semantic_label == self.semantic_ignore_index:
+                if (semantic_label == self.semantic_ignore_index) or semantic_label == self.ignore_index:
                     continue
                 mask = (semantic_labels == semantic_label)
                 loss += centroid_instance_loss(outputs[mask], labels[mask], subbatch_indices[mask], weights[mask], self.use_weights, self.ignore_index, self.normalize, self.delta_d, self.delta_v)