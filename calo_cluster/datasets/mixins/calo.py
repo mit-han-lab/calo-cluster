73ec19ab8bfad1a119b4a56b0a40585baa6e1ca9
Alex Schuy <alexjschuy@gmail.com>
2021-10-13 19:27:58 +0000

    Add eta bounds to calo data.

--- calo_cluster/datasets/mixins/calo.py
+++ calo_cluster/datasets/mixins/calo.py
@@ -14,9 +14,21 @@ class CaloDataModule(PandasDataModule):
     noise_id: int
     min_hits_per_cluster: int
     min_cluster_energy: float
+    min_eta: float
+    max_eta: float
+
+    def __post_init__(self):
+        super().__post_init__()
+        if self.min_eta is None and self.max_eta is None:
+            self.transformed_data_dir = transformed_data_dir = self.data_dir / \
+                f'min_energy_{self.min_cluster_energy}_min_hits_{self.min_hits_per_cluster}'
+        else:
+            self.transformed_data_dir = transformed_data_dir = self.data_dir / \
+                f'min_energy_{self.min_cluster_energy}_min_hits_{self.min_hits_per_cluster}_eta_{self.min_eta}_{self.max_eta}'
+        transformed_data_dir.mkdir(exist_ok=True, parents=True)
 
     @classmethod
-    def _apply_transform(cls, event_path: Path, min_cluster_energy: float, min_hits_per_cluster: int, noise_id: int, transformed_data_dir: Path):
+    def _apply_transform(cls, event_path: Path, min_cluster_energy: float, min_hits_per_cluster: int, noise_id: int, min_eta: float, max_eta: float, transformed_data_dir: Path):
         event = pd.read_pickle(event_path)
         tracks = cls.get_clusters(event)
 
@@ -26,6 +38,9 @@ class CaloDataModule(PandasDataModule):
             tracks['clusterId'] == noise_id)]
         event = event[event['PFcluster0Id'].isin(tracks['clusterId'])]
         event = event.reset_index(drop=True)
+
+        event = event[event['eta'] > min_eta]
+        event = event[event['eta'] < max_eta]
 
         out_path = transformed_data_dir / event_path.name
         if event.shape[0] > 0:
@@ -52,6 +67,4 @@ class CaloDataModule(PandasDataModule):
         return pd.concat([energy, eta, phi, nconstituents], axis=1).reset_index().rename(columns={cluster_col: 'clusterId'})
 
     def get_transform_function(self):
-        transformed_data_dir = self.raw_data_dir / \
-            f'min_energy_{self.min_cluster_energy}_min_hits_{self.min_hits_per_cluster}'
-        return partial(self._apply_transform, min_cluster_energy=self.min_cluster_energy, min_hits_per_cluster=self.min_hits_per_cluster, noise_id=self.noise_id, transformed_data_dir=transformed_data_dir)
+        return partial(self._apply_transform, min_cluster_energy=self.min_cluster_energy, min_hits_per_cluster=self.min_hits_per_cluster, min_eta=self.min_eta, max_eta=self.max_eta, noise_id=self.noise_id, transformed_data_dir=self.transformed_data_dir)
